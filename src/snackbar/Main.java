ffd87a6b88c6a3154cdeb5d2f2375f4789ee4d15
David White <dwhite0627@gmail.com>
2021-01-27 16:29:43 -0500

    Instanstiate main objects

---
+++ src/snackbar/Main.java
@@ -0,0 +1,40 @@
+package snackbar;
+
+// class imports
+import snackbar.Snack;
+import snackbar.Customer;
+import snackbar.VendingMachine;
+
+public class Main {
+  public static void main(String[] args) {
+    // Variables
+    int maxCustomerId = 0;
+    int maxSnackId = 0;
+    int maxVendingId = 0;
+
+    // Customers
+    Customer customer1 = new Customer(maxCustomerId, "Jane", 45.25);
+    maxCustomerId++;
+
+    Customer customer2 = new Customer(maxCustomerId, "Bob", 33.14);
+    maxCustomerId++;
+
+    // Vending machines
+    VendingMachine foodVending = new VendingMachine(maxVendingId, "Food");
+    maxVendingId++;
+
+    VendingMachine drinkVending = new VendingMachine(maxVendingId, "Drink");
+    maxVendingId++;
+
+    VendingMachine officeVending = new VendingMachine(maxVendingId, "Office");
+    maxVendingId++;
+
+    // Vending machine snacks
+    Snack chips = new Snack(maxSnackId, "Chips", 36, 1.75, foodVending.getId());
+    Snack chocolateBar = new Snack(maxSnackId, "Chocolate bar", 36, 1.00, foodVending.getId());
+    Snack pretzel = new Snack(maxSnackId, "Pretzel", 30, 2.00, foodVending.getId());
+    Snack soda = new Snack(maxSnackId, "Soda", 24, 2.50, drinkVending.getId());
+    Snack water = new Snack(maxSnackId, "Water", 20, 2.75, drinkVending.getId());
+
+  }
+}
